acfbc6e0f9682faa730654d2b09730297b3dd041
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2021-05-09 22:39:30 +0300

    Update main.java

--- main.java
+++ main.java
@@ -6,7 +6,7 @@ public static void main(String[] args){
 Tribe tribe= new Tribe();
 
 List<List<String>> records = new ArrayList<>();
-try (BufferedReader br = new BufferedReader(new FileReader("book.csv"))) {
+try (BufferedReader br = new BufferedReader(new FileReader("tribal-king-selection-test1.csv"))) {
     String line;
     while ((line = br.readLine()) != null) {
         String[] values = line.split(COMMA_DELIMITER);
