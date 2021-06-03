4ab591dae4984e915ec214dc68f5ab68c38a33c4
Baris000eng <>
2021-06-03 17:29:00 +0000

    ABC

--- Main.java
+++ Main.java
@@ -21,7 +21,6 @@ public class Main {
   public static int a=0;
   public static int b=0;
  
-   
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter test name: ");
